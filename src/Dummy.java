904d2c318ff5755ff712ad9fbfd59c280c439e9a
AbdulSameer <shaiksameer15@gmail.com>
2024-09-05 17:17:18 +0530

    browser file changes with get url

--- src/Dummy.java
+++ src/Dummy.java
@@ -8,5 +8,6 @@ public class Dummy {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.get("https://google.com");
     }
 }
