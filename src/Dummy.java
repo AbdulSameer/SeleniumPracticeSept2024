8d002e04c7ae988b2f1c9e96c73b3aa9a940450a
AbdulSameer <shaiksameer15@gmail.com>
2024-09-05 16:44:10 +0530

    browser file changes

--- src/Dummy.java
+++ src/Dummy.java
@@ -1,9 +1,12 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class Dummy {
     public static void main(String args[]){
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     }
 }
