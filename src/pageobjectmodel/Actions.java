5f7d286243ada007de3b443a876125d0e12807cb
AbdulSameer <Shaiksameer15@gmail.com>
2025-02-03 02:01:21 +0530

    fluent wait and webdriver wait code added

--- src/pageobjectmodel/Actions.java
+++ src/pageobjectmodel/Actions.java
@@ -1,12 +1,29 @@
 package pageobjectmodel;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+
+import static pageobjectmodel.Base.driver;
 
 public class Actions {
 
         locators l = new locators();
         public  Actions(){
-            PageFactory.initElements(Base.driver,l);
+            PageFactory.initElements(driver,l);
+            WebElement ele = driver.findElement(By.xpath(""));
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+            wait.until(ExpectedConditions.visibilityOf(ele));
+
+            Wait<WebDriver> wait1 = new FluentWait<>(driver)
+                    .withTimeout(Duration.ofSeconds(15))
+                    .pollingEvery(Duration.ofMillis(10))
+                    .ignoring(NoSuchElementException.class);
         }
 
         public void close(){
